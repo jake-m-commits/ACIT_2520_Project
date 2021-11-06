30358d7e8af9d92bf4fad0a886a521dfbe14ddaf
Jake Martin <jm9357481@gmail.com>
2021-11-05 21:16:50 -0700

    fixes/updates listOne reminder function to work with authenticated user

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -12,13 +12,16 @@ let remindersController = {
 
   listOne: (req, res) => {
     let reminderToFind = req.params.id;
-    let searchResult = database.cindy.reminders.find(function (reminder) {
+    //console.log(req);
+    console.log(req.params.id);
+    let searchResult = req.user.reminders.find(function (reminder) {
       return reminder.id == reminderToFind;
     });
+    console.log(searchResult);
     if (searchResult != undefined) {
       res.render("reminder/single-reminder", { reminderItem: searchResult });
     } else {
-      res.render("reminder/index", { reminders: database.cindy.reminders });
+      res.render("reminder/index", { reminders: req.user.reminders });
     }
   },
 
