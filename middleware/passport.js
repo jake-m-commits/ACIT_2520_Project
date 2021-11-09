ee4d2e4fca26e50aadf24219a52b77941d5e1aaf
Jake Martin <jm9357481@gmail.com>
2021-11-08 16:09:09 -0800

    add GitHub auth [WIP]

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,6 +1,10 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const GitHubStrategy = require("passport-github2").Strategy;
 const userController = require("../controller/userController");
+const process = require("process");
+const dots = require("dotenv").config();
+
 const localLogin = new LocalStrategy(
   {
     usernameField: "email",
@@ -14,6 +18,20 @@ const localLogin = new LocalStrategy(
       : done(null, false, {
           message: "Your login details are not valid. Please try again",
         });
+  }
+);
+
+const GitHubLogin = new GitHubStrategy(
+  {
+    clientID: process.env.GH_CLIENT_ID,
+    clientSecret: process.env.GH_CLIENT_SECRET,
+    callbackURL: "http://localhost:3001/auth/github/callback",
+  },
+  function (accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      let user = userController.getUserByGitHubIdOrCreate(profile.id);
+      return done(null, user);
+    });
   }
 );
 
@@ -33,3 +51,4 @@ passport.deserializeUser(function (id, done) {
 });
 
 module.exports = passport.use(localLogin);
+module.exports = passport.use(GitHubLogin);
